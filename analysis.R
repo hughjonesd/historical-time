8aed563d35e534ff7b54d86141a33cce01855749
David Hugh-Jones <davidhughjones@gmail.com>
2018-11-19 13:13:55 +0000

    turn graphs into functions

--- analysis.R
+++ analysis.R
@@ -11,98 +11,116 @@ library(ggridges)
 # == data cleaning =============
 
 # all matching "XXXX"
-years <- readr::read_tsv("googlebooks-years", col_names = c("to", "from", "matches", "vols"))
+years <- readr::read_tsv("googlebooks-years", col_names = c("to", "from", "count", "vols"))
+#
+years <- complete(years, to, nesting(from), fill = list(count = 0, vols = 0))
 
 # all matching "year XXXX"
-years_rob <- readr::read_tsv("googlebooks-years-robust", col_names = c("to", "from", "matches", "vols"))
+years_rob <- readr::read_tsv("googlebooks-years-robust", col_names = c("to", "from", "count", "vols"))
 years_rob$to <- readr::parse_number(years_rob$to)
+years_rob <- complete(years_rob, to, nesting(from), fill = list(count = 0, vols = 0))
 
 # from hansard-clean.R
 years_han <- readRDS("hansard_year_counts.rds")
 years_han %<>% 
       as_tibble(rownames = "from") %>% 
       mutate(from = as.numeric(from)) %>% 
-      tidyr::gather("to", "matches", -from, convert = TRUE)
-years_han %<>% 
+      tidyr::gather("to", "count", -from, convert = TRUE) %>% 
       group_by(from) %>% 
-      filter(sum(matches) > 0)
+      filter(sum(count) > 0)
 
-years <- years_han
-years <- years_rob
+years <- full_join(years, years_rob, by = c("from", "to"), suffix = c("", "_rob"))
+years <- full_join(years, years_han, by = c("from", "to"), suffix = c("", "_han"))
 
-years$diff <- years$from - years$to
+years <- tidyr::gather(years, source, count, matches("count"), matches("vols")) %>% 
+      mutate(source = recode(source,
+        "count"      = "ngrams",
+        "count_han"  = "hansard",
+        "count_rob"  = "ngrams_year",
+        "vols"       = "ngrams_vols",
+        "vols_rob"   = "ngrams_year_vols"
+      ))
+
 years %<>% 
-      group_by(from) %>% 
+      group_by(from, source) %>% 
       mutate(
-        prop_matches = matches/sum(matches),
-#        prop_vols    = vols/sum(vols),
-        decade       = 10 * floor(from/10)
-      )
+        count  = if (source == "hansard" && (from < 1803 || from > 2005)) count else 
+                       tidyr::replace_na(count, 0),
+        diff   = from - to,
+        prop   = count/sum(count),
+        prop_past = if_else(diff >= 0, count/sum(count[diff >= 0]), NA_real_),
+        decade = 10 * floor(from/10)
+      ) %>% 
+      tidyr::drop_na() %>% 
+      select(source, to, from, decade, diff, everything())
 
 
 year_qs <- years %>% 
       filter(to <= from) %>% 
-      arrange(from, to) %>% 
-      group_by(from) %>% 
+      arrange(source, from, to) %>% 
+      group_by(from, source) %>% 
       do({
         to <- .$to
-        pm <- .$matches
-        qs <- Hmisc::wtd.quantile(to, weights = pm, probs = 0:10/10)
-        names(qs) <- 0:10*10
+        count <- .$count
+        qs <- try(Hmisc::wtd.quantile(to, weights = count, probs = 0:10/10), silent = TRUE)
+        if (inherits(qs, "try-error")) qs <- rep(NA, 11)
+        names(qs) <- 0:10 * 10
         as.data.frame(rbind(qs))
       }) %>% 
-      tidyr::gather("quantile", "year", -from, convert = TRUE)
+      tidyr::gather("quantile", "year", -from, -source, convert = TRUE) %>% 
+      mutate(
+        diff = from - year
+      )
 
 # == plots =================
 
-# nice:
-ggplot(years, aes(x = from, y = to)) + 
-      geom_raster(aes(fill=log(matches))) +
-      scale_fill_viridis_c(option = "C") +
-      theme_minimal() + ylim(1800, 2000)
+ggplot2::theme_set(theme_minimal())
 
-years %>% 
-      filter(to %in% 1913:1915, from >= 1900) %>% 
-      ggplot(aes(x = from, y = prop_matches, group = to, 
-        colour = factor(to))) + 
-      geom_line() + 
-      scale_y_log10()
+raster_plot <- function (mysource, y = to, dep_var) {
+  dep_var <- enquo(dep_var)
+  y <- enquo(y)
+  years %>% 
+        filter(source == mysource) %>% 
+        ggplot(aes(x = from, y = !! y)) + 
+          geom_raster(aes(fill = log(!! dep_var))) +
+          scale_fill_viridis_c(option = "C") 
+}
 
-year_qs %>% 
-      filter(
-        from >= 1800,
-        quantile %in% c(10, 20, 50, 80, 90)
-      ) %>% 
-      ggplot(aes(
-        x = from, 
-        y = year - from, 
-        colour = factor(quantile), 
-        group = factor(quantile)
-      )) + 
-      geom_line(alpha = 0.6) + 
-      geom_smooth(method = "loess", span = 0.1, se = FALSE) + 
-      scale_color_viridis_d() +
-      theme_minimal() +
-      scale_x_continuous(breaks = seq(1800, 2000, 20)) 
-# not sure if log is appropriate?
+ref_year_plot <- function(mysource, ref_years, y = prop_past) {
+  y <- enquo(y)
+  years %>% 
+    filter(to %in% ref_years, source == mysource) %>% 
+    ggplot(aes(x = from, y = !!y, group = to, 
+      colour = factor(to))) + 
+    geom_line()
+}
 
+backwards_plot <- function (mysource, from_years, y = prop_past) {
+  y <- enquo(y)
+  years %>% 
+    filter(from %in% from_years, source == mysource) %>% 
+    ggplot(aes(x = to, y = !!y, group = from, 
+      colour = factor(from))) + 
+    geom_line()
+}
 
-years %>% 
-      group_by(decade, to) %>% 
-      # filter(to <= from) %>% 
-      summarize(
-        # prop_vols    = mean(prop_vols),
-        prop_matches = mean(prop_matches)
-      ) %>% 
-      filter(decade >= 1900, to >= 1800) %>% 
-      ggplot(aes(x = to, y = factor(decade), height = prop_matches)) +
-        geom_density_ridges(
-          stat = "identity", 
-          fill = "black", 
-          color = "white",
-          rel_min_height = 0.01
-        ) + 
-        theme_ridges() + xlim(1800, 2000)
+quantile_plot <- function(mysource, quantiles = c(20, 50, 80)) {
+  year_qs %>% 
+        filter(
+          from >= 1800,
+          source == mysource,
+          quantile %in% quantiles
+        ) %>% 
+        ggplot(aes(
+          x = from, 
+          y = diff, 
+          colour = factor(quantile), 
+          group = factor(quantile)
+        )) + 
+        geom_line(alpha = 0.6) + 
+        geom_smooth(method = "loess", span = 0.1, se = FALSE) + 
+        scale_color_viridis_d()
+}
 
 
 # == analyses =====================
